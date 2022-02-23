0d2e4ff988a8f0b3f465982c3d8c4380f29a078f
Dmytro Fesenko <dmitryfesenko92@gmail.com>
2022-02-23 14:54:42 +0200

    openvpn module

--- main.tf
+++ main.tf
@@ -29,7 +29,35 @@ module "vpc" {
 
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Terraform = "true"
   }
+}
+
+
+resource "aws_route53_zone" "test_r53z0ne" {
+  name = var.r53domain_name
+
+  vpc {
+    vpc_id = module.vpc.vpc_id
+  }
+
+  tags = {
+    Name = "test_r53z0ne"
+  }
+}
+
+
+module "openvpn" {
+  source = "anugnes/openvpn/aws"
+
+  ami               = var.openvpn.ami
+  domain            = var.r53domain_name
+  ebs_region        = var.region
+  ebs_size          = var.openvpn.ebs_size
+  instance_type     = var.openvpn.instance_type
+  key_name          = var.aws_key_name
+  public_subnet_ids = module.vpc.public_subnets
+  route_zone_id     = aws_route53_zone.test_r53z0ne.id
+  vpc_cidr          = var.cidrList.vpc.cidr
+  vpc_id            = module.vpc.vpc_id
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "access_key" {}
 variable "secret_key" {}
 
+variable "aws_key_name" {}
+variable "aws_key_path" {}
+
 
 variable "region" {
   default = "us-east-2"
@@ -11,5 +14,18 @@ variable "cidrList" {
     vpc         = { name = "test-vpc", cidr = "10.14.0.0/16" }
     public_sub  = { name = "test-sub-public", cidr = "10.14.1.0/24" }
     private_sub = { name = "test-sub-private", cidr = "10.14.2.0/24" }
+  }
+}
+
+variable "r53domain_name" {
+  default = "test.com"
+}
+
+variable "openvpn" {
+  default = {
+    ami           = "ami-0fb653ca2d3203ac1"
+    ebs_size      = 8
+    instance_type = "t2.micro"
+
   }
 }
